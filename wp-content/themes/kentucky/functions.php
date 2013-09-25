69ccc39b7a4943d3d24ec87f76857a9af96ce01e
Dominic <dinter@id-on.de>
2013-09-25 17:40:15 +0200

    h3

--- wp-content/themes/kentucky/functions.php
+++ wp-content/themes/kentucky/functions.php
@@ -80,8 +80,8 @@ function kentucky_widgets_init() {
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'kentucky_widgets_init' );
